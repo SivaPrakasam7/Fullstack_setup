0422ac61863f3715621d8a270886a4d98702b782
Sivaprakasam7 <prakasams22@gmail.com>
2025-03-08 11:12:18 +0530

    deployment trigger

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -65,7 +65,7 @@ app.use(errorHandler);
 
 if (process.env.NODE_ENV !== 'test')
     app.listen(port, () => {
-        console.log(`Server running on http://localhost:${port}`);
+        console.log(`Server running on ${port}`);
     });
 
 export default app;
