444dc31fcbf501c0cb880bc82d765bb99086d7e5
Sivaprakasam7 <prakasams22@gmail.com>
2025-03-18 14:39:08 +0530

    Table component update

--- packages/frontend-react/src/app/components/table.tsx
+++ packages/frontend-react/src/app/components/table.tsx
@@ -1,9 +1,10 @@
-import { ReactNode, useState } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
 import { generateKey } from 'services/repository/utils';
+import { TextField } from './form/textField';
 
 export const Table = ({ data, columns }: ITable) => {
     const [currentPage, setCurrentPage] = useState(1);
-    const itemsPerPage = 10; // Adjust this value as needed
+    const [itemsPerPage, setItemsPerPage] = useState(10);
 
     // Calculate total pages
     const totalPages = Math.ceil(data.length / itemsPerPage);
@@ -41,67 +42,104 @@ export const Table = ({ data, columns }: ITable) => {
         return Array.from({ length: end - start + 1 }, (_, i) => start + i);
     };
 
+    useEffect(() => {
+        setCurrentPage(1);
+    }, [data.length]);
+
     return (
-        <div className="relative overflow-x-auto rounded-md border no-scrollbar flex flex-col bg-white">
-            <table className="w-full text-sm text-left rtl:text-right rounded-md">
-                <thead className="bg-gray-300 sticky top-0">
-                    <tr>
-                        {columns.map((c) => (
-                            <th
-                                key={`headcol-${generateKey()}`}
-                                scope="col"
-                                className="px-4 py-4 truncate"
-                            >
-                                {c.headerCell ? c.headerCell : c.header}
-                            </th>
-                        ))}
-                    </tr>
-                </thead>
-                <tbody>
-                    {paginatedData.length ? (
-                        paginatedData.map((row) => (
-                            <tr
-                                key={`tablerow-${generateKey()}`}
-                                className={`bg-white dark:bg-gray-800 hover:bg-blue-50 border-b dark:border-gray-700 border-gray-200`}
-                            >
-                                {columns.map((c) => (
-                                    <td
-                                        key={`tablecell-${generateKey()}`}
-                                        className={`px-4 py-4 overflow-hidden w-fit ${c.cellClass ? c.cellClass : ''}`}
-                                    >
-                                        {c.cell
-                                            ? c.cell({
-                                                  value: row[c.field],
-                                                  row,
-                                              })
-                                            : row[c.field]}
-                                    </td>
-                                ))}
-                            </tr>
-                        ))
-                    ) : (
-                        <tr className="bg-white dark:bg-gray-800 hover:bg-blue-50">
-                            <td
-                                scope="row"
-                                colSpan={columns.length}
-                                className="px-6 py-10 font-medium col-span-6 text-center text-2xl text-gray-400"
-                            >
-                                No data found
-                            </td>
+        <>
+            <div className="relative overflow-x-auto !overflow-y-clip rounded-[4px] border no-scrollbar flex flex-col bg-white">
+                <table className="w-full text-sm text-left rtl:text-right rounded-[4px]">
+                    <thead className="bg-primary-200 sticky top-0 bg-gray-200 dark:bg-gray-700 dark:text-gray-400">
+                        <tr>
+                            {columns.map((c) => (
+                                <th
+                                    key={`headcol-${generateKey()}`}
+                                    scope="col"
+                                    className="px-4 py-4 truncate"
+                                >
+                                    {c.headerCell ? c.headerCell : c.header}
+                                </th>
+                            ))}
                         </tr>
-                    )}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        {paginatedData.length ? (
+                            paginatedData.map((row, index) => (
+                                <tr
+                                    key={`tablerow-${generateKey()}`}
+                                    className={`bg-white dark:bg-gray-800 hover:bg-blue-50 border-b dark:border-gray-700 border-gray-200 odd:light:bg-white even:bg-gray-100 even:dark:bg-gray-900`}
+                                >
+                                    {columns.map((c) => (
+                                        <td
+                                            key={`tablecell-${generateKey()}`}
+                                            className={`px-4 py-4 overflow-hidden w-fit ${c.cellClass ? c.cellClass : ''}`}
+                                        >
+                                            {c.cell
+                                                ? c.cell({
+                                                      value: row[c.field],
+                                                      row: {
+                                                          ...row,
+                                                          index:
+                                                              (currentPage -
+                                                                  1) *
+                                                                  itemsPerPage +
+                                                              index +
+                                                              1,
+                                                      },
+                                                  })
+                                                : row[c.field]}
+                                        </td>
+                                    ))}
+                                </tr>
+                            ))
+                        ) : (
+                            <tr className="bg-white dark:bg-gray-800 hover:bg-blue-50">
+                                <td
+                                    scope="row"
+                                    colSpan={columns.length}
+                                    className="px-6 py-10 font-medium col-span-6 text-center text-2xl text-gray-400"
+                                >
+                                    No data found
+                                </td>
+                            </tr>
+                        )}
+                    </tbody>
+                </table>
+            </div>
             {data.length > 0 && (
                 <div className="flex gap-2 items-center justify-between p-2">
                     <p className="text-sm font-bold">Total : {data.length}</p>
                     <nav>
                         <ul className="flex items-center -space-x-px h-10 text-base">
                             <li>
+                                <TextField
+                                    startIcon={
+                                        <p className="text-gray-500 text-nowrap ml-2 -mr-3">
+                                            Rows :
+                                        </p>
+                                    }
+                                    name="month"
+                                    type="select"
+                                    value={`${itemsPerPage}`}
+                                    size="p-1.5 pl-4"
+                                    layoutClass="max-w-32"
+                                    className="!w-fit mr-2"
+                                    onChange={(e) => setItemsPerPage(e.value)}
+                                    options={[
+                                        { id: '10', label: '10' },
+                                        { id: '25', label: '25' },
+                                        { id: '50', label: '50' },
+                                        { id: '100', label: '100' },
+                                    ]}
+                                    noError={true}
+                                />
+                            </li>
+                            <li>
                                 <button
                                     onClick={goToPreviousPage}
                                     disabled={currentPage === 1}
-                                    className={`flex items-center justify-center px-4 h-10 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white ${
+                                    className={`flex items-center justify-center px-3 h-10 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white ${
                                         currentPage === 1
                                             ? 'cursor-not-allowed opacity-50'
                                             : ''
@@ -143,7 +181,7 @@ export const Table = ({ data, columns }: ITable) => {
                                 <button
                                     onClick={goToNextPage}
                                     disabled={currentPage === totalPages}
-                                    className={`flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white ${
+                                    className={`flex items-center justify-center px-3 h-10 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white ${
                                         currentPage === totalPages
                                             ? 'cursor-not-allowed opacity-50'
                                             : ''
@@ -171,7 +209,7 @@ export const Table = ({ data, columns }: ITable) => {
                     </nav>
                 </div>
             )}
-        </div>
+        </>
     );
 };
 
--- packages/services/constants/regex.ts
+++ packages/services/constants/regex.ts
@@ -4,3 +4,5 @@ export const passwordRegex =
 export const fullNameRegex = '^(?=.{2,30}$)[a-zA-Z]+(?:[\\s-][a-zA-Z]+)*$';
 export const phoneNumberRegex =
     '^\\+?[1-9]\\d{0,2}[-.\\s]?\\(?\\d{1,4}\\)?[-.\\s]?\\d{1,4}[-.\\s]?\\d{1,9}$';
+export const phoneNumberRegexWithCountryCode =
+    '^\\+[1-9]\\d{0,2}[-.\\s]?\\(?\\d{1,4}\\)?[-.\\s]?\\d{1,4}[-.\\s]?\\d{1,9}$';
